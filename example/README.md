8778d4540d0bc41abbc5332d756e6fea926675fe
emadeddineibo <emadeddin.eibo@gmail.com>
2020-05-09 23:51:43 +0300

    Readme & examples

--- example/README.md
+++ example/README.md
@@ -1,16 +1,69 @@
-# example
+# Animated Stack
 
-Example for Animated Stack Widget.
+Custom animated stacked pages, to create customisable side menu buttons.
+Providing simple menu for sharing buttons, side menu, message sending, about page, ... etc.
+
+![](example1.gif) ![](example2.gif)
 
 ## Getting Started
+At first, you have to create Foreground and Background Widgets.
 
-This project is a starting point for a Flutter application.
+Here is a simple example to meet minimum requirements.
 
-A few resources to get you started if this is your first Flutter project:
+```dart
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Stack Demo',
+      theme: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      home: AnimatedStack(
+        backgroundColor: Colors.white,
+        fabBackgroundColor: Colors.black,
+        foregroundWidget: ..., // Main page
+        columnWidget: ..., // Right widget, preferably Column
+        bottomWidget: ...,// Right widget, preferably Row
+      ),
+    );
+  }
+}
+```
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+## Complete Features
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Here's all what you can customize in this widget!
+
+## Button attributes
+
+```dart
+AnimatedStack(
+    backgroundColor: ...,     // Background Container color
+    foregroundWidget: ...,    // Foreground Container color
+    
+    columnWidget: ...,        // Right Widget, (use Column for best results)
+    bottomWidget: ...,        // Bottom Widget, (use Row for best results)
+    
+    scaleHeight: 100,         // Sliding animation height, default is 60
+    scaleWidth: 100,          // Sliding animation width, default is 60
+    
+    
+    buttonIcon: ...,          // FAB icon (IconData)
+    fabIconColor: ...,        // FAB icon color
+    animateButton: false,     // Sometimes, we don't want to animate the button!
+    fabBackgroundColor: ...,  // FAB background color
+    
+    slideAnimationDuration: ..., // Animation Duration, default is 800 Milliseconds
+    buttonAnimationDuration: ...,// Animation Duration, default is 240 Milliseconds
+    
+    openAnimationCurve: ...,    // Animation Curve, default is ElasticOutCurve(0.9)
+    closeAnimationCurve: ...,   // Animation Curve, default is ElasticInCurve(0.9)
+    );
+```
+
+### For complete examples, check example folder
+
+## License
+MIT License.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:animated_stack/animated_stack.dart';
 import 'package:flutter/material.dart';
-import 'icon_tile.dart';
-import 'item_placeholder.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -35,25 +32,25 @@ class MyApp extends StatelessWidget {
           child: ListView.builder(
             itemCount: 5,
             itemBuilder: (context, index) {
-              return ItemPlaceholder();
+              return _ItemPlaceholder();
             },
           ),
         ),
         columnWidget: Column(
           children: <Widget>[
-            IconTile(
+            _IconTile(
               width: 100,
               height: 60,
               iconData: Icons.share,
             ),
             SizedBox(height: 20),
-            IconTile(
+            _IconTile(
               width: 60,
               height: 60,
               iconData: Icons.image,
             ),
             SizedBox(height: 20),
-            IconTile(
+            _IconTile(
               width: 60,
               height: 60,
               iconData: Icons.camera_alt,
@@ -74,3 +71,101 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class _IconTile extends StatelessWidget {
+  final double width;
+  final double height;
+  final IconData iconData;
+
+  const _IconTile({Key key, this.width, this.height, this.iconData})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: Color(0xff645478),
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Icon(
+        iconData,
+        color: Color(0xffAEA6B6),
+      ),
+    );
+  }
+}
+
+class _ItemPlaceholder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ClipOval(
+            child: Container(
+              width: 60,
+              height: 60,
+              color: Color(0xff9783A9),
+            ),
+          ),
+          SizedBox(width: 15),
+          Expanded(
+            child: Container(
+              height: 120,
+              decoration: BoxDecoration(
+                color: Color(0xff6D528D),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: _RowPlaceholder(color: 0xffA597B4),
+                      width: MediaQuery.of(context).size.width * 2 / 5,
+                    ),
+                    _RowPlaceholder(color: 0xff846CA1),
+                    _RowPlaceholder(color: 0xff846CA1),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _RowPlaceholder extends StatelessWidget {
+  final int color;
+
+  const _RowPlaceholder({Key key, this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 15,
+      decoration: BoxDecoration(
+        color: Color(color),
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+    );
+  }
+}
+
